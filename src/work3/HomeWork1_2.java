75ea527b7f5fe7ec4f26f3888680af24ca4952ad
moon <s81112612@gmail.com>
2024-12-30 23:31:02 +0800

    feat:my homework

--- src/work3/HomeWork1_2.java
+++ src/work3/HomeWork1_2.java
@@ -7,39 +7,40 @@ import java.util.Scanner;
 public class HomeWork1_2 {
 	public static void main(String[] args) {
 		System.out.println("輸入三個數字");
-		
-		//取得輸入的數字
-		Scanner sc =new Scanner(System.in); 
-		
-		//宣告陣列用於存放三角形的三個邊長
-		int[] triangleSides=new int[3];
-		
+
+		// 取得輸入的數字
+		Scanner sc = new Scanner(System.in);
+
+		// 宣告陣列用於存放三角形的三個邊長
+		int[] triangleSides = new int[3];
+
 		// 使用 for 迴圈將輸入的三個數字放進陣列
-		for(int i=0;i<triangleSides.length;i++) 
-			triangleSides[i]=sc.nextInt();
-		
-		//將陣列內的元素進行排序，方便後續判斷
+		for (int i = 0; i < triangleSides.length; i++)
+			triangleSides[i] = sc.nextInt();
+
+		// 將陣列內的元素進行排序，方便後續判斷
 		Arrays.sort(triangleSides);
-		
+
 		// 建立 HomeWork1_2 物件
-		HomeWork1_2 triangleType =new HomeWork1_2();
-		//使用物件呼叫 checkTriangleType 方法進行三角形類型判斷並取回傳值
+		HomeWork1_2 triangleType = new HomeWork1_2();
+		// 使用物件呼叫 checkTriangleType 方法進行三角形類型判斷並取回傳值
 		String result = triangleType.checkTriangleType(triangleSides);
-		//印出回傳值結果
+		// 印出回傳值結果
 		System.out.println(result);
-		
+
 		sc.close();
 	}
+
 	String checkTriangleType(int[] sides) {
-		if(sides[0]<=0||sides[1]<=0||sides[2]<=0) {
-			return "不是三角形" ;
-		}else if(sides[0]==sides[1]&& sides[1]==sides[2]) {
+		if (sides[0] <= 0 || sides[1] <= 0 || sides[2] <= 0 || (sides[0] + sides[1] <= sides[2])) {
+			return "不是三角形";
+		} else if (sides[0] == sides[1] && sides[1] == sides[2]) {
 			return "正三角形";
-		}else if(sides[0]==sides[1]&&sides[0]!=sides[2]) {
+		} else if (sides[0] == sides[1] && sides[0] != sides[2]) {
 			return "等腰三角形";
-		}else if(Math.pow(sides[0], 2)+Math.pow(sides[1], 2)==Math.pow(sides[2], 2)) {
+		} else if (Math.pow(sides[0], 2) + Math.pow(sides[1], 2) == Math.pow(sides[2], 2)) {
 			return "直角三角形";
-		}else {
+		} else {
 			return "其他三角形";
 		}
 	}
