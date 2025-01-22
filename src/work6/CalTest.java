778a24f375e20b6ce6e2285d7b25a6ac13c446b6
moons1225 <mooniey1225@gmail.com>
2025-01-22 23:58:15 +0800

    我還是有完成作業的

--- src/work6/CalTest.java
+++ src/work6/CalTest.java
@@ -1,7 +1,32 @@
 package work6;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class CalTest {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		Calculator calculator = new Calculator();
+		boolean isSuccess = false;
+		while (!isSuccess) {
+			try {
+				System.out.println("請輸入x的值");
+				int x = sc.nextInt();
+				
+				System.out.println("請輸入y的值");
+				int y = sc.nextInt();
+
+				int result = calculator.powerXY(x, y);
+				System.out.println(x + "的" + y + "次方等於" + result);
+				isSuccess = true;
+			} catch (CalException e) {
+				System.out.println(e.getMessage());
+				 sc.nextLine();
+			}catch(InputMismatchException e) {
+				System.out.println("輸入格式不正確");
+				 sc.nextLine();
+			}
+		}
+		sc.close();
 	}
 }
--- src/work6/Calculator.java
+++ src/work6/Calculator.java
@@ -1,5 +1,15 @@
 package work6;
 
 public class Calculator {
-	
+	private int x ;
+	private int y ;
+	public int powerXY(int x, int y) throws CalException{
+		if(x == 0 && y==0) 
+			throw new CalException("0的0次方沒有意義！");
+		if(y<0) 
+			throw new CalException("次方為負值，回傳結果不為整數");
+
+		return (int)Math.pow(x,y);
+		
+	}
 }
